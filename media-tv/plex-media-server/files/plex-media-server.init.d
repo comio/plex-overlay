ad847d305c98c2a13f4aff88d638c76412cba06b
Luigi 'Comio' Mantellini <luigi.mantellini@gmail.com>
2021-08-30 17:34:40 +0200

    media-tv/plex-media-server: Fix init.d init script

--- media-tv/plex-media-server/files/plex-media-server.init.d
+++ media-tv/plex-media-server/files/plex-media-server.init.d
@@ -2,7 +2,7 @@
 
 start() {
 	ebegin "Starting Plex Media Server"
-	start-stop-daemon -S -m -p ${PLEX_PIDFILE} -1 ${PLEX_OUTLOG} -2 ${PLEX_ERRLOG} --quiet -u ${PLEX_USER} -N -5 -b --exec ${PLEX_SCRIPT}
+	start-stop-daemon -S -m -p ${PLEX_PIDFILE} -1 ${PLEX_OUTLOG} -2 ${PLEX_ERRLOG} --quiet -u ${PLEX_USER} -N -5 -b --exec "${PLEX_SCRIPT}"
 	eend $?
 }
 
